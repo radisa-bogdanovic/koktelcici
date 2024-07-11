6c7dba9dd7f9988fd33273821e27fe80d625bad5
radisa.bogdanovic <radisa.bogdanovic@algometric.net>
2024-07-11 20:15:30 +0200

    fixes on footer

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -1,7 +1,9 @@
 function Footer() {
   return (
     <div style={{ padding: "8px", backgroundColor: "#000", color: "#fff" }}>
-      <p>Ovo je vezbanje od strane Radise godine {new Date().getFullYear()}</p>
+      <p style={{ textAlign: "center" }}>
+        Ovo je vezbanje od strane Radise godine {new Date().getFullYear()}
+      </p>
     </div>
   );
 }
